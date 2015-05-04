3238e12c71ce907a93914305ea481c6976052eb4
YuR <YuR@OCIO--J2NNHQ1.US.SINET.SI.EDU>
2015-05-04 16:19:32 -0400

    Add PDF suuport

--- src/edu/si/CDIS/CIS/ImageFilePath.java
+++ src/edu/si/CDIS/CIS/ImageFilePath.java
@@ -6,6 +6,7 @@
 package edu.si.CDIS.CIS;
 
 import edu.si.CDIS.utilties.DataProvider;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,27 +19,37 @@ import edu.si.CDIS.CDIS;
 import edu.si.CDIS.DAMS.Database.SiAssetMetaData;
 import edu.si.CDIS.CIS.Database.CDISTable;
 import edu.si.CDIS.StatisticsReport;
+
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // This is the main entrypoint for syncing the image file and image file path in TMS
 public class ImageFilePath {
-    
+	private final static Logger logger = Logger.getLogger(CDIS.class.getName());
+	
     Connection tmsConn;
     Connection damsConn;
-    int IDSPathId;
     int successfulUpdateCount;
-    
+  
+    int IDSPathId;
+    int PDFPathId;
+    String fileType;
+    String uan;
+        
     public void sync(CDIS cdis_new, StatisticsReport StatReport) {
+    	
+    	logger.log(Level.FINER, "ImageFilePath.sync()");
         //assign the database connections for later use
         this.tmsConn = cdis_new.tmsConn;
         this.damsConn = cdis_new.damsConn;
         this.IDSPathId = Integer.parseInt(cdis_new.properties.getProperty("IDSPathId"));
+        this.PDFPathId = Integer.parseInt(cdis_new.properties.getProperty("PDFPathId"));
         
         ArrayList<Integer> neverSyncedCDISIdLst = new ArrayList<Integer>();
 
         //Get list of renditions to sync
         neverSyncedCDISIdLst = getNeverSyncedImagePath();
-        
+
         CDISTable cdisTbl = new CDISTable();
         
         //loop through the list, and update the pathname
@@ -62,8 +73,10 @@ public class ImageFilePath {
                      "and a.SyncIDSPathDate is NULL " +
                      "and a.MetaDataSyncDate is not NULL " +
                      "and c.isColor = 1 " +
-                     "and b.PathID != " + this.IDSPathId;
+                     "and b.PathID != " + this.IDSPathId + " " + 
+                     "and b.PathID != " + this.PDFPathId;       
         
+
         System.out.println("IDS syncPath Select: " + sql);
         
         ResultSet rs;
@@ -91,6 +104,7 @@ public class ImageFilePath {
     }
     
    
+    
     // That the list of renditions by CDIS_ID and loop through them one at a time, and perform synchronizations
     private void processRenditionList(ArrayList<Integer> CDISIdList, StatisticsReport StatRpt) {
 
@@ -134,13 +148,19 @@ public class ImageFilePath {
                         cdisTbl.setUOIID(rs.getString("UOIID"));
                     }
                     
-                    //get UAN from DAMS
-                    String uan = getUAN(cdisTbl);
-                    
+                    //get UAN and file type from DAMS
+//                    String uan = getUAN(cdisTbl);
+                    boolean goodFileType = getUANFileType(cdisTbl);
+                    if (!goodFileType) {
+                    	logger.log(Level.SEVERE, "Fatal Error, ImageFilePath.processRenditionList: CDIS only support TIF, JPG, and PDF file type");
+                    	throw new Exception("CDIS only support TIF, JPG, and PDF file type");
+                    }
+                                       
                     int updateCount = 0;
                     
                     // update the FilePath and FileName with the path and UAN
-                    updateCount = updateFilePath(cdisTbl, uan); 
+//                    updateCount = updateFilePath(cdisTbl, uan); 
+                    updateCount = updateFilePath(cdisTbl, this.uan, this.fileType); 
                     
                     // If se updated the filepath successfully, then log the transaction in the CDIS table
                     if (updateCount > 0) { 
@@ -184,13 +204,49 @@ public class ImageFilePath {
     }
     
     // This function returns the current UAN from DAMS for the current UOIID
-    private String getUAN (CDISTable cdisTbl) {
+//    private String getUAN (CDISTable cdisTbl) {
+//        
+//        String uan = null;
+//        
+//        String sql = "select OWNING_UNIT_UNIQUE_NAME " +
+//                    "from SI_ASSET_METADATA " +
+//                    "where uoi_id = '" + cdisTbl.getUOIID() + "'";
+//        
+//        System.out.println("get UAN: " + sql);
+//        
+//        ResultSet rs = null;
+//        PreparedStatement stmt = null;
+//
+//        try {
+//            stmt = damsConn.prepareStatement(sql);                                
+//            rs = stmt.executeQuery();
+//        
+//            if (rs.next()) {
+//                uan = rs.getString(1);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try { if (rs != null) rs.close(); } catch (SQLException se) { se.printStackTrace(); }
+//            try { if (stmt != null) stmt.close(); } catch (SQLException se) { se.printStackTrace(); }
+//        }
+//        
+//        return uan;
+//        
+//    }
+   
+    
+    private boolean getUANFileType (CDISTable cdisTbl) {
         
         String uan = null;
-        
-        String sql = "select OWNING_UNIT_UNIQUE_NAME " +
-                    "from SI_ASSET_METADATA " +
-                    "where uoi_id = '" + cdisTbl.getUOIID() + "'";
+        String fileType = null;
+ 
+        String sql = "select a.OWNING_UNIT_UNIQUE_NAME, substr(b.NAME,INSTR(NAME, '.',-1)) " +
+        			"from SI_ASSET_METADATA a, UOIS b " +
+        			"where a.uoi_id = b.uoi_id " +
+        			"and b.uoi_id = '" + cdisTbl.getUOIID() + "'";
+
         
         System.out.println("get UAN: " + sql);
         
@@ -203,6 +259,7 @@ public class ImageFilePath {
         
             if (rs.next()) {
                 uan = rs.getString(1);
+                fileType = rs.getString(2);
             }
 
         } catch (Exception e) {
@@ -212,19 +269,58 @@ public class ImageFilePath {
             try { if (stmt != null) stmt.close(); } catch (SQLException se) { se.printStackTrace(); }
         }
         
-        return uan;
+        this.uan = uan;
+        this.fileType = fileType;
+        
+        if (fileType.equalsIgnoreCase("TIF")) {
+        	return true;
+        }
+        
+        if (fileType.equalsIgnoreCase("JPG")) {
+        	return true;
+        }
+        
+        if (fileType.equalsIgnoreCase("PDF")) {
+        	return true;
+        }
+        
+        return false;      
         
     }
     
+    
+
+    
     //This function updates the filePath and filename in TMS to point to the IDS derivative
-    private int updateFilePath (CDISTable cdisTbl, String uan){
+//    private int updateFilePath (CDISTable cdisTbl, String uan){
+//        
+//        String sql = "update MediaFiles " +
+//                    "set PathID = " + this.IDSPathId + ", " +
+//                    "FileName = '" + uan + "' " +
+//                    "where RenditionID = " + cdisTbl.getRenditionId();
+//        
+//        System.out.println("IDS update: " + sql);
+//        
+//        int updateCount = DataProvider.executeUpdate(this.tmsConn, sql);
+//        
+//        return updateCount;
+//        
+//    }
+    
+    private int updateFilePath (CDISTable cdisTbl, String uan, String fileType){
+    	
+    	int pathId = this.IDSPathId;
+    	if (fileType.equalsIgnoreCase("PDF")) {
+    		pathId = this.PDFPathId;
+    	}
+    	
+    	String sql = "update MediaFiles " +
+    				"set PathID = " + pathId + ", " +
+    				"FileName = '" + uan + "' " +
+    				"where RenditionID = " + cdisTbl.getRenditionId();
         
-        String sql = "update MediaFiles " +
-                    "set PathID = " + this.IDSPathId + ", " +
-                    "FileName = '" + uan + "' " +
-                    "where RenditionID = " + cdisTbl.getRenditionId();
-        
-        System.out.println("IDS update: " + sql);
+ //       System.out.println("IDS/PDF update: " + sql);
+        logger.log(Level.FINER, "ImageFilePath.updateFilePath() " + "sql = " + sql);
         
         int updateCount = DataProvider.executeUpdate(this.tmsConn, sql);
         
