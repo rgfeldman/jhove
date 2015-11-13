72da51bfe634b8ca8e32993de5a8cb4ab149d57f
rfeldman <rfeldman@quotient-inc.com>
2015-11-13 09:04:01 -0500

    SendToIngest->SendToHotFolder

--- src/edu/si/CDIS/utilties/XmlSqlConfig.java
+++ src/edu/si/CDIS/utilties/XmlSqlConfig.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.si.CDIS;
+package edu.si.CDIS.utilties;
 
+import edu.si.CDIS.CDIS;
 import edu.si.CDIS.CDIS;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
