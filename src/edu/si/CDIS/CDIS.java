4dcc3e3a749e73f7dca8c171e05aabce53ab551f
rfeldman <rfeldman@quotient-inc.com>
2016-08-24 12:26:26 -0400

    linkToDAMS does not need config xml file

--- src/edu/si/CDIS/CDIS.java
+++ src/edu/si/CDIS/CDIS.java
@@ -315,15 +315,17 @@ public class CDIS {
             }
             CDIS.siHoldingUnit = collectionGrp.getSiHoldingUnit(); 
             
-            // read the XML config file
-            XmlSqlConfig xml = new XmlSqlConfig();    
-            boolean xmlReturn = xml.read(CDIS.getCollectionGroup(), CDIS.getOperationType());
-            if (! xmlReturn) {
-                logger.log(Level.SEVERE, "Fatal Error: unable to read/parse sql xml file");
-                return;
+            if (! CDIS.getOperationType().equals("linkToDAMS") ) {
+                // read the XML config file
+                XmlSqlConfig xml = new XmlSqlConfig();    
+                boolean xmlReturn = xml.read(CDIS.getCollectionGroup(), CDIS.getOperationType());
+                if (! xmlReturn) {
+                    logger.log(Level.SEVERE, "Fatal Error: unable to read/parse sql xml file");
+                    return;
+                }
+                // save the queries in a Node List
+                CDIS.queryNodeList = xml.getOpQueryNodeList();
             }
-            // save the queries in a Node List
-            CDIS.queryNodeList = xml.getOpQueryNodeList();
             
             switch (CDIS.operationType) {
                 
