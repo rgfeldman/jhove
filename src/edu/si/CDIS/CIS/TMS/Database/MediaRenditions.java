7c733aa7313e55a5702e3e25aefde6e4e10aaec6
rfeldman <rfeldman@quotient-inc.com>
2016-09-29 15:49:39 -0400

    #250

--- src/edu/si/CDIS/CIS/TMS/Database/MediaRenditions.java
+++ src/edu/si/CDIS/CIS/TMS/Database/MediaRenditions.java
@@ -103,7 +103,7 @@ public class MediaRenditions {
                         "1, " +
                         "'CDIS', " +
                         "CURRENT_TIMESTAMP, " +
-                        getRemarks() +
+                        "'" + getRemarks() + "', " +
                         mediaStatusID + ", " +
                         "CONVERT (date,SYSDATETIME()))";
         
